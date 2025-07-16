396c496e2f1f50f5a8ed29999e2c950234e3f3eb
sharathkannan <138484653+sharath-kannan@users.noreply.github.com>
2025-07-16 19:42:13 +0530

    Update creativecloud/blocks/cc-forms/cc-forms.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- creativecloud/blocks/cc-forms/cc-forms.js
+++ creativecloud/blocks/cc-forms/cc-forms.js
@@ -41,7 +41,7 @@ const formConfig = {
       customValue: 'region,timezone',
       imsAddressMailValue: 'postalcode,state,country',
       rengaUDSValue: 'orgsize',
-      id:123,
+      id: 123,
       userProfileValue: 'country,fname,lname,phonenumber,email,orgname,jobfunction,industry',
       ...odinConfig,
     },
